05a799cffa7e7349aedf81b7a5292c561b0dad3a
tkhsutah <tkhsutah@gmai.com>
2024-12-09 06:52:45 +0000

    [finish] 3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,13 @@
 class ListsController < ApplicationController
   def new
+    # viewへ渡すためのインスタンス変数に空のmodelオブジェクトを生成
+    @list = List.new
+  end
+
+  def create
+    list = List.new(list_params) #データを受け取って新規登録するインスタンス
+    list.save #データベースに保村
+    redirect_to '/top' # topへリダイレクト
   end
 
   def index
@@ -9,5 +17,10 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
